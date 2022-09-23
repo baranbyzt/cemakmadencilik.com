1cd1a55411566cf0fad6fe56b52ad4f2fd304b36
byztmain <byztmain2121@gmail.com>
2022-09-24 00:41:20 +0300

    v1

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+
+
+let FF = () => {
+burgermenu.style.display = "flex"
+
+}
+
+
+let burgermenu = document.getElementById("burgermenu");
+
+
+
+let burgerburger = document.getElementById("burgerburger");
+
+burgerburger.addEventListener("click", function() {
+   
+    if(burgermenu.style.display == 'none'){
+        burgermenu.style.display = "flex"
+    }
+    else{
+        burgermenu.style.display = "none"
+    }
+  });
+
+
+  let süpereminem = document.getElementById('süpereminem');
+
+  süpereminemesdf.addEventListener("click", function() {
+   
+    if(süpereminem.style.display == 'none'){
+        süpereminem.style.display = "flex"
+    }
+    else{
+        süpereminem.style.display = "none"
+    }
+  });
